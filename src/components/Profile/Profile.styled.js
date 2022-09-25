db9940fcc42a929ba18e96cc3bf51a8b100b0b75
Maksi16 <maksim.moroz.kr@gmail.com>
2022-09-25 19:17:30 +0300

    Profile

---
+++ src/components/Profile/Profile.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Card = styled.profile;
+
+export const Description = styled.description;
+
+export const Avatar = styled.avatar;
+
+export const Tag = styled.tag;
